49b962f9db4662f6a4cfb5dfaf58af670bd1f0bc
nitya <nityanandabehera85132@gmail.com>
2022-03-26 19:45:48 +0530

    first commit

---
+++ djangoauthapi/account/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from account.serializers import UserRegistrationSerializer
+
+
+class UserRegistrationView(APIView):
+    def post(self, request, formate=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response({'msg': "Registration sucessful"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Create your views here.
